da47b2c98e5134fcee3829cb4133e27881619e3d
Naila <zahwanaila27@gmail.com>
2025-12-08 04:33:00 -0800

    Refactor tree creation and item insertion logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,39 @@ using namespace std;
 
 int main()
 {
-    adrLemari root, p;
+    adrLemari root, p, akar, atasan, bawahan;
     infotype x;
     int i, n, level;
 
     cout << "Masukkan input size: ";
     cin >> n;
-    createTreeL(root);
-    x.nama = "lemari";
+    createRootL(root);
+    x.nama = "Lemari";
     x.id = 80;
     x.kategori = "ruang";
     x.jenis = "wadah";
     x.harga = 500000;
-    p = createItemP(x);
-    insertItemP(root, p);
+    akar = createItemRoot(x);
+    //insertItemP(root, p);
+
+    createRootL(root);
+    x.nama = "Atasan";
+    x.id = 80;
+    x.kategori = "-";
+    x.jenis = "wadah";
+    x.harga = 0;
+    atasan = createItemRoot(x);
+    //insertItemP(root, p);
+
+    createRootL(root);
+    x.nama = "Bawahan";
+    x.id = 80;
+    x.kategori = "-";
+    x.jenis = "wadah";
+    x.harga = 0;
+    bawahan = createItemRoot(x);
+    //insertItemP(root, p);
+
     for (i = 1; i <= n; i++) {
         cout << "Masukkan id pakaian: ";
         cin >> x.id;
@@ -30,21 +49,17 @@ int main()
         cin >> x.jenis;
         cout << "Masukkan harga pakaian: ";
         cin >> x.harga;
-        p = createItemP(x);
-        insertItemP(root, p);
-        cout << endl;
-    }
-    cout << "Tampilkan berdasarkan traversal Pre-Order: ";
-    readPreOrder(root);
+        if (x.kategori == "Atasan") {
+            p = createItemP(x);
+            insertItemAtasan(root, p);
+        } else if (x.kategori == "Bawahan") {
+            p = createItemP(x);
+            insertItemBawahan(root, p);
+        };
+    };
+    InsertRoot(akar, atasan, bawahan);
     cout << endl;
-    cout << "\nTampilkan berdasarkan traversal In-Order: ";
-    readInOrder(root);
-    cout << endl;
-    cout << "\nTampilkan berdasarkan traversal Post-Order: ";
-    readPostOrder(root);
-    cout << endl;
-    cout << "\nTampilkan berdasarkan traversal Level-Order: ";
-    readLevelOrder(root, level);
+    menu(root);
     cout << endl;
     return 0;
 }
